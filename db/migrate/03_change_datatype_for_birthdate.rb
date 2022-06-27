e348d2dcc9922fed987e2cb5b04c3a89781c0d6f
MissDine <enidnyatichi1212@gmail.com>
2022-06-27 15:16:27 +0300

    Completed assignment

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[6.1]
     def change
+        # Changing a colum-use change_column
       change_column :students, :birthdate, :datetime
     end
   end
