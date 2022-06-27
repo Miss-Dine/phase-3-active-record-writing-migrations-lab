24bae6f0bdfc90ca8d01314e8d176fae4ee8cdc0
MissDine <enidnyatichi1212@gmail.com>
2022-06-27 15:26:05 +0300

    Completed assignment

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[6.1]
     def change
-        # add column with-add_column
+        # adding column with-add_column
       add_column :students, :grade, :interger
       add_column :students, :birthdate, :string
     end
